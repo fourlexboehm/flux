f3d79d3a2eda106a4e56f5675d85f81930beaae6
Alex <Alex>
2026-01-18 13:28:34 +1030

    Revert "deps: vendor clap-zig-bindings into deps/clap-bindings"
    This reverts commit 4000a7c9c9b65016c2058b5233c47f41f227efa5.

--- deps/clap-bindings/src/audio_buffer.zig
+++
@@ -1,9 +0,0 @@
-pub const AudioBuffer = extern struct {
-    /// either data32 or data64 will be set.
-    data32: ?[*][*]f32,
-    data64: ?[*][*]f64,
-    channel_count: u32,
-    /// latency from/to the audio interface.
-    latency: u32,
-    constant_mask: u64,
-};
