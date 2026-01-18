4000a7c9c9b65016c2058b5233c47f41f227efa5
Alex <Alex>
2026-01-18 12:51:41 +1030

    deps: vendor clap-zig-bindings into deps/clap-bindings

---
+++ deps/clap-bindings/build.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    // this allows other code that depends on this
+    // one to not have to re-declare the module.
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const clap_bindings_module = b.addModule("clap-bindings", .{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const test_step = b.step("test", "Run unit tests");
+    const tests = b.addTest(.{
+        .root_module = clap_bindings_module,
+    });
+    const run_tests = b.addRunArtifact(tests);
+    test_step.dependOn(&run_tests.step);
+    b.default_step = test_step;
+
+    const doc_step = b.step("doc", "Generate autodocs");
+    const docs_test = b.addObject(.{
+        .name = "clap-bindings",
+        .root_module = clap_bindings_module,
+    });
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = docs_test.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+    doc_step.dependOn(&install_docs.step);
+}
