7482d0e8395a85e4c4ea48c5888ecbecd0de0aa4
frettedfeline <frettedfeline@users.noreply.github.com>
2026-02-01 18:42:33 +1030

    Share clap plugin type

--- shared/root.zig
+++ shared/root.zig
@@ -2,6 +2,7 @@ pub const clap_entry = @import("clap_entry.zig");
 pub const core = @import("core.zig");
 pub const gui = @import("gui.zig");
 pub const imgui_style = @import("imgui_style.zig");
+pub const plugin_extensions = @import("plugin_extensions.zig");
 
 pub const ext = struct {
     pub const audioports = @import("ext/audioports.zig");
