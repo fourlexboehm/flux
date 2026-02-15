91885e8915819277b25893dff3dfb5189cf03e6e
Alex <Alex>
2026-02-15 13:39:32 +1030

    Fix ui scaling on linux

--- shared/imgui_style.zig
+++ shared/imgui_style.zig
@@ -6,6 +6,10 @@ pub fn applyScaleFromMemory(font: []const u8, scale: f32) void {
     zgui.getStyle().scaleAllSizes(scale);
 }
 
+pub fn applyFontFromMemory(font: []const u8, scale: f32) void {
+    _ = zgui.io.addFontFromMemory(font, std.math.floor(16.0 * scale));
+}
+
 pub fn applyScaleFromFile(path: [:0]const u8, scale: f32) void {
     _ = zgui.io.addFontFromFile(path, std.math.floor(16.0 * scale));
     if (scale != 1.0) {
